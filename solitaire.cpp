02589ab8c54e00a18a73fa624c066dceb4ddc3bb
Nico Swart <jns1999@gmail.com>
2021-03-22 22:10:58 -0400

    Update solitaire.cpp
    Move pop_back()

--- solitaire.cpp
+++ solitaire.cpp
@@ -138,11 +138,11 @@ std::tuple<bool, vector<enum PINTYPE> > find_solution(vector<enum PINTYPE> &pos)
    while (!sol_path.empty() && !solution)
    {
       pos = sol_path.back();
+      sol_path.pop_back();
       solution = solution_p(pos, solution_pos);
       if (!solution)
       {
         vector< pair<int,int> > moves = get_moves(pos);
-        sol_path.pop_back();
         for (auto &move : moves)
         {
             sol_path.push_back(make_move(pos, move));
